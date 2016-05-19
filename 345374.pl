d27d47b0abea3c921f4b17318a867fee2283d327
Leonid Logvinov <logvinov.leon@gmail.com>
2016-05-19 17:21:09 +0200

    Program reading and input validation

--- 345374.pl
+++ 345374.pl
@@ -1,2 +1,18 @@
-verify(N, Program).
-main :- verify(1, 'peterson'), writef('%t\n', "Hello World").
+:- ensure_loaded(library(lists)).
+
+safe_verify(N, Str) :-
+    read(Str, Vars),
+    read(Str, Arrays),
+    read(Str, Code),
+    close(Str), 
+    write([Vars, Arrays, Code, N]), nl.
+
+verify(N, Program) :-
+    (integer(N), N > 0) -> 
+    (
+        catch(open(Program, read, Str), _, 
+            (write('Error: brak pliku o nazwie - '), write(Program), fail)),
+        safe_verify(N, Str)
+    );
+    (write('Error: parametr 0 powinien byc liczba > 0\n'), fail).
+main :- verify(1, 'unsafe.txt').
